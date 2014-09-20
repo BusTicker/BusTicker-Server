4f39532e6072f55e1c2e23e6cc23ac9106eeb34b
tdhsmith <contact@tdhsmith.com>
2014-09-20 11:22:04 -0500

    Add routes file, include Morgan logging

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+// Expresssss
 var express = require('express');
 var app = express();
 
+// Configure app properties
 app.set('port', process.env.PORT || process.env.npm_package_config_port || 3000);
 
+// Start logger
+var logger = require('morgan');
+app.use(logger('dev'));
+
+// Bring in external routes
 var routes = require(__dirname + '/routes');
-app.use('/', routes);
+//app.use('/', routes);
+
 
 console.log("Go go gadget server.");
 
+// Launch the server
 var server = app.listen(app.get('port'), function() {
     console.log('Listening on port %d', server.address().port);
 });
+
