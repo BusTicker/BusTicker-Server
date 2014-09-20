a6b2760fb0eb59f8209b9b7b7078bd2264d0dbe4
tdhsmith <contact@tdhsmith.com>
2014-09-20 15:47:17 -0500

    Add haversine distance function

--- utilities/utils.js
+++ utilities/utils.js
@@ -6,4 +6,19 @@ utilities.to8601 = function (timeStr) {
   return Date.parse(time);
 }
 
+// Reasonable approximation of ground distances based on lat&lon
+// As described by census.gov. Thanks to http://andrew.hedges.name/experiments/haversine/
+utilities.haversineDistance = function(lat1, lon1, lat2, lon2) {
+  var d2r = function (degrees) {return degrees*Math.PI/180;};
+  lat1 = d2r(lat1);
+  lat2 = d2r(lat2);
+  lon1 = d2r(lon1);
+  lon2 = d2r(lon2);
+  var dlon = lon2 - lon1;
+  var dlat = lat2 - lat1;
+  var a = Math.pow((Math.sin(dlat/2)),2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow((Math.sin(dlon/2)),2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a) );
+  return (3958.7 * c);
+}
+
 module.exports = utilities;
