7963e06635593c81853b2ed51cd792eb548bff3b
tdhsmith <contact@tdhsmith.com>
2014-09-20 14:39:29 -0500

    Provide dummy results to stops route. (partially done)

--- routes/stops.js
+++ routes/stops.js
@@ -3,6 +3,11 @@ var configure = function(utils) {
   var express = require('express');
   var router = express.Router();
 
+  function getMockData(res) {
+    var mockData = require("sample data/dummy_stops.json");
+    res.json(mockData);
+  }
+
   router.get('/',function(req,res) {res.send("STOPS");});
   return router;
 }
