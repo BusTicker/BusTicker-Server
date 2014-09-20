bea14c1850e8931d458585f1b346a93862925277
tdhsmith <contact@tdhsmith.com>
2014-09-20 11:11:29 -0500

    Dummy app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || process.env.npm_package_config_port || 3000);
+
+var routes = require(__dirname + '/routes')
+app.use('/', routes);
+
+console.log("Go go gadget server.");
+
+var server = app.listen(app.get('port'), function() {
+    console.log('Listening on port %d', server.address().port);
+});
